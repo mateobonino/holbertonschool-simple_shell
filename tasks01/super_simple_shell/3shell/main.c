138cb80daa2fa2d842f6c098ec0134c8414faf03
mateo <mateo@localhost.localdomain>
2022-08-01 11:19:12 -0400

    test

--- tasks01/super_simple_shell/3shell/main.c
+++ tasks01/super_simple_shell/3shell/main.c
@@ -2,6 +2,26 @@
 
 int main(void)
 {
-	shell_loop;
+	char *prompt = NULL;
+	char **commands = NULL;
+	char **ret_cmd = NULL;
+	_puts(C_OUTPUT);
+	while (1)
+	{
+		_puts(_getenv("USER"));
+		_putchar('@');
+		_puts("sh> ");
+		prompt = readline();
+		if (prompt == "\n")
+		{
+			free(prompt);
+			continue;
+		}
+		commands = token_line(prompt);
+		ret_cmd = cmd_check(commands);
+		free(prompt);
+		free(commands);
+		free(ret_cmd);
+	}
 	return (0);
 }
--- tasks01/super_simple_shell/3shell/main.h
+++ tasks01/super_simple_shell/3shell/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-extern char **environ;
 #define C_OUTPUT "\033[H\033[J"
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,21 +11,13 @@ extern char **environ;
 #include <limits.h>
 #include <sys/stat.h>
 
+extern char **environ;
 char *_strdup(char *str);
-char *line_read(void);
 char *_getenv(const char *name);
-void print_prompt(void);
-char **line_parse(char *prompt);
-int cmd_checker(char **commands);
-void path_exc(char **commands);
-int is_builtin(char **commands);
-char *command_verify(char **commands);
-int exec(char **commands);
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 void _puts(char *str);
 int _putchar(char c);
-void run_built(char **commands);
 
 #endif /*MAIN_H*/
